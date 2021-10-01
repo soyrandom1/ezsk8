7abdac176eeed7c765cd759399cf9f2380b0d417
AlbanoBoschi <albanob24@gmail.com>
2021-10-01 12:14:45 -0300

    index
    Primer index

---
+++ Index/main.js
@@ -0,0 +1,7 @@
+ScrollReveal().reveal('.Sup');
+ScrollReveal().reveal('.nav-main');
+ScrollReveal().reveal('.Armar_Skate', {delay: 500});
+ScrollReveal().reveal('.Carrusel_Skate', {delay: 500});
+ScrollReveal().reveal('.news-cards', {delay: 500});
+ScrollReveal().reveal('.card-full', {delay: 500});
+ScrollReveal().reveal('.social', {delay: 500});
